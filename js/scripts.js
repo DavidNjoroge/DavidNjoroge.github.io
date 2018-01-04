13395e7c83baa0cd4b9cb4b82bbe9f833b311f9c
David Mwangi <njorogedavid13@gmail.com>
2018-01-04 11:40:30 +0300

    added images

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// var header = $("#header")
+// var somm = document.getElementById('header')
+// var pattern = Trianglify({
+
+//   variance:0.5,
+//   cell_size: 20});
+
+// // header.appendChild(pattern.canvas());
+// // somm.style['background-image'] = 'url('+pattern.png()+')'
+// $("#header").css('background-image', 'url('+pattern.png()+')')
+$(document).ready(function () {
+    $(function () {
+        $('.tlt').textillate();
+    })
+});
